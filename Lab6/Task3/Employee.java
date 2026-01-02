dd8d7efe4c516104769504859531f72cb740804d
asmaabe01 <asmaaebequi@gmail.com>
2026-01-02 18:41:03 +0800

    added extended classes

---
+++ Lab6/Task3/Employee.java
@@ -0,0 +1,21 @@
+package Task3;
+
+public class Employee  extends Person {
+
+    private String office;
+    private double salary;
+    private MyDate dateHired;
+
+    public Employee(String name, String address, String phoneNumber, String emailAddress, String office, double salary, MyDate dateHired) {  
+        super(name, address, phoneNumber, emailAddress); 
+        this.office = office;
+        this.salary = salary;
+        this.dateHired = dateHired;
+    } 
+
+    @Override    
+    public String toString() {
+        return "Employee: " + super.toString() + ", Office: " + office + ", Salary: " + salary + ", Date Hired: " + dateHired;
+    }
+    
+}
