0feb237fb85b7f059efe83b7781aa93c7c752c2e
asmaabe01 <asmaaebequi@gmail.com>
2026-01-03 16:15:06 +0800

    Fix bug in GeometricObject

---
+++ Lab6/Task5/TestTriangle.java
@@ -0,0 +1,13 @@
+package Task5;
+
+public class TestTriangle {
+    public static void main(String[] args){
+
+        Triangle tr1= new Triangle(3,4,5);
+
+        System.out.println(tr1);
+        System.out.println("its area is: "+tr1.getArea());
+        System.out.println("its perimeter is: "+tr1.getPerimeter());
+    }
+    
+}
